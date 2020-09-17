73d5e59751d25b9b7500c7148c79a100a93bc87d
Sang Hun <oper6909@gmail.com>
2020-09-18 02:24:29 +0900

    Binary Search practice. 2020.09.18

--- Programmers/ImmiChk.py
+++ Programmers/ImmiChk.py
@@ -3,7 +3,7 @@ times = [7,10]
 
 def solution(n, times):
     answer = 0
-    lft, rgt = 1, n*times[0]
+    lft, rgt = 1, n*min(times)
     while lft <= rgt:
         mid = (lft+rgt) >> 1
         cnt = sum([mid//t for t in times])
